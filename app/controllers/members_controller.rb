6afc9cc50784c4fb612044ab0cde0ec2ff0b63f6
Alex Tideman <alex.tideman@gmail.com>
2015-09-21 11:45:10 -0600

    Add Volunteer and Sponsor index page, add How Effortful Works button for modal

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,20 @@
 class MembersController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
