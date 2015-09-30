ff009babfc4618e054469eea4c27864979a1f568
Alex Tideman <alex.tideman@gmail.com>
2015-09-30 17:34:22 -0600

    Update Effort index to 3 columns based on request reward

--- app/controllers/efforts_controller.rb
+++ app/controllers/efforts_controller.rb
@@ -1,6 +1,21 @@
 class EffortsController < ApplicationController
 
   def index
-    @efforts = Effort.all
+    efforts = Effort.all
+    @pending_efforts = efforts.map do |effort|
+      effort if effort.schedule.nil?
+    end.compact
+
+    @food_efforts = @pending_efforts.map do |effort|
+      effort if effort.requested_reward == "Food"
+    end.compact
+
+    @clothing_efforts = @pending_efforts.map do |effort|
+      effort if effort.requested_reward == "Clothing"
+    end.compact
+
+    @job_efforts = @pending_efforts.map do |effort|
+      effort if effort.requested_reward == "Job Resources"
+    end.compact
   end
 end
